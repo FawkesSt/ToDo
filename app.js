069f1f7f9159d744c97404b71d0eb95c337b86d0
Matheus Santos Ribeiro <matheussantos@Matheuss-MacBook-Air.local>
2023-03-08 11:14:53 -0300

    Added js logic to delete and complete a todo

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const todoBtn = document.querySelector(".todo-btn");
 const todos = document.querySelector(".todos");
 const toast = document.querySelector(".toast-alert");
 
+
+
 //Event listener
 todoBtn.addEventListener("click", addTodo);
+todos.addEventListener("click", deleteCheck);
+
 
 function addTodo(event) {
   event.preventDefault();
@@ -35,16 +39,40 @@ function addTodo(event) {
 
     const completBtn = document.createElement("button");
     completBtn.classList.add("complete-btn");
-    completBtn.innerText = "Complete";
+    completBtn.innerText = "Mark as done";
     buttons.appendChild(completBtn);
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("delete-btn");
-    deleteBtn.innerHTML = `<spam class="bi bi-trash">Delete</spam>`;
+    deleteBtn.innerHTML = `Delete<i class="bi bi-trash"></i>`;
     buttons.appendChild(deleteBtn);
-
+  
     todos.appendChild(todoDiv);
+    todoInput.value = "";
 
     
   }
+}
+
+
+function deleteCheck(e){
+  const item = e.target;
+
+  //Delete todo
+  if(item.classList[0] === "delete-btn"){
+    const todo = item.parentElement;
+    todoParent = todo.parentElement;
+    todoParent.remove();
+    console.log(todoParent);
+  }
+
+  if(item.classList[0] === "complete-btn"){
+    const todo = item.parentElement;
+    todoParent = todo.parentElement;
+    todoParent.classList.add("completed");
+    console.log(item);
+    item.innerHTML = `Done<i class="bi bi-check"></i>`;
+    
+  }
+  
 }
